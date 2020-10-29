ce9569503b4ff4546e9ed3688b5e6ffcde35ce15
taru <taruru1010@gmail.com>
2020-10-29 09:09:32 +0900

    add julius-start.sh

---
+++ julius-start.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# julius -C julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf > /dev/null &
+julius -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/am-gmm.jconf -module > /dev/null &
+# julius -C ./julius-4.4.2/julius-kit/grammar-kit-4.3.1/hmm_mono.jconf -C ./julius-4.4.2/julius-kit/grammar-kit-4.3.1/hmm_ptm.jconf -module > /dev/null &
+
+
+echo $!
+sleep 2
