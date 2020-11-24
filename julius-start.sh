05e2ff3c2f696e0c53fbafd41150f4b293629a34
taru <taruru1010@gmail.com>
2020-11-24 20:13:53 +0900

    change jconf path

--- julius-start.sh
+++ julius-start.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
 # julius -C julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf > /dev/null &
-julius -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/am-gmm.jconf -module > /dev/null &
+# julius -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/main.jconf -C ./julius-4.4.2/julius-kit/dictation-kit-v4.3.1-linux/am-gmm.jconf -module > /dev/null &
+julius -C ./dictation-kit-v4.3.1-linux/main.jconf -C ./dictation-kit-v4.3.1-linux/am-gmm.jconf -module > /dev/null &
+
 # julius -C ./julius-4.4.2/julius-kit/grammar-kit-4.3.1/hmm_mono.jconf -C ./julius-4.4.2/julius-kit/grammar-kit-4.3.1/hmm_ptm.jconf -module > /dev/null &
 
 
