7d207b6c67f7afee06d71d1745dc5e7df6a62ae6
tsuji <t.satoki111@gmail.com>
2020-12-17 12:02:36 +0900

    fix

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import subprocess
 import socket
 import line_notice
 import voice
-from time import sleep
 import psutil
+import os
+import time
+import datetime
+import shutil
+import glob
 
 
 host = "localhost"
@@ -49,28 +53,30 @@ def get_picture():
     except:
         return "Command envailed."
 
+store_dir_name = "images"
+
 def store_image(name):
-    filename = "{}-{}-{}.jpg".format(time.time(), datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), name)
-    shutil.copyfile("image.jpg", "./images/{}".format(filename))
-    #print(filename)
+    try:
+        os.makedirs(store_dir_name, exist_ok=True)
+        file_name = "{}-{}-{}.jpg".format(time.time(), datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), name)
+        shutil.copyfile("image.jpg", "{}/{}".format(store_dir_name, file_name))
+        return True
+    except:
+        return False
 
 def search_image(name):
-    filelist = sorted(glob.glob("./images/*{}.jpg".format(name)))
-    X = 255
-    Y = 255
-    print(filelist[-1])
-    root = tk.Tk()
-    root.title("TsukushiSpeaker")
-    root.minsize(X, Y)
-    image = tk.PhotoImage(file=filelist[-1])
-    canvas = tk.Canvas(bg="white", width=X, height=Y)
-    canvas.place(x=0, y=0)
-    canvas.create_image(0, 0, image=image, anchor=tk.NW)
-    root.mainloop()
+    file_list = sorted(glob.glob("{}/*{}.jpg".format(store_dir_name, name)))
+    if file_list:
+        return file_list[-1]
+    return ""
 
-def delete_images():
-    shutil.rmtree("./images/")
-    os.mkdir("./images/")
+def clean_images():
+    try:
+        shutil.rmtree("{}".format(store_dir_name))
+        os.makedirs(store_dir_name, exist_ok=True)
+        return True
+    except:
+        return False
 
 def wait_for_OK():
     try:
@@ -88,14 +94,14 @@ def wait_for_OK():
                         line = line[index+6:line.find('"', index+6)]
                         recog_text = recog_text + line
                 print("認識結果: " + recog_text)
+                get_picture()
                 store_image(recog_text)
-                #search_image("検索するファイル名")
-                #delete_images()
+                #search_image("検索する物体名")
+                #clean_images()
                 # wake word
                 if "ＯＫ" in recog_text:
                     print("exec")
                     speech2Line()
-                    get_picture()
 
                 data = ""
             else:
