a2c1dab40550c7c7312dab54b147c626aa61df23
allgemein <amanetamada@gmail.com>
2021-01-02 17:09:50 +0900

    firefoxで動くようにした

--- GUI.py
+++ GUI.py
@@ -3,4 +3,4 @@ from main import search_image
 eel.init("GUI")
 search_image = eel.expose(search_image)
 
-eel.start("home.html")
+eel.start("home.html", mode="custom", cmdline_args=['firefox', '-url', 'localhost:8000/home.html'])
