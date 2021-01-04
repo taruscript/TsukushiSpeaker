d5a5a420bc1cb9f0dfae1385b26ca25090eb5306
allgemein <amanetamada@gmail.com>
2021-01-04 19:15:48 +0900

    検索した結果の画像を取得して表示できるようにした

--- GUI.py
+++ GUI.py
@@ -1,6 +1,15 @@
-import eel
+import eel, base64
 from main import search_image
 eel.init("GUI")
-search_image = eel.expose(search_image)
+
+def img_to_b64(path):
+    with open(path, "rb") as image:
+        return base64.b64encode(image.read())
+
+@eel.expose
+def export_image(name):
+    path = search_image(name)
+    image = img_to_b64(path).decode('utf-8')
+    return image
 
 eel.start("home.html", mode="custom", cmdline_args=['firefox', '-url', 'localhost:8000/home.html'])
